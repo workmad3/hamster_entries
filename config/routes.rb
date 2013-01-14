6ccce8d1ec0795d460a1fa110226e449a4074a36
David Workman <workmad3@gmail.com>
2013-01-14 23:30:58 +0000

    Devise signin
    Added a combined sign-in and sign-up page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 HamsterEntries::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: { sign_in: "login", sign_up: "login"}
   root to: "registrations#new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
