20339052a81c0074763a83c58aff89a29a0c33e1
David Workman <workmad3@gmail.com>
2013-01-20 15:23:00 +0000

    Added a controller to handle the root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 HamsterEntries::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_up: "login"}
-  root to: "registrations#new"
+  root to: "dashboards#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
