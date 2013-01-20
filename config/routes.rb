f96bf33a13f9e6d4135a6aad48706a9e3f6737d1
David Workman <workmad3@gmail.com>
2013-01-20 15:41:19 +0000

    Added stub for shows list. Added some extra detail to the top dropdowns

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 HamsterEntries::Application.routes.draw do
   devise_for :users, path_names: { sign_in: "login", sign_up: "login"}
+  resources :shows
   root to: "dashboards#show"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
